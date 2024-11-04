8868d8c55d1c24e75b5657c9988c47f0125aa983
Joshua McNabb <joshuamcnabb1995@gmail.com>
2024-11-04 13:03:36 -0700

    add chakraui

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
 import "./demos/ipc";
+import { Provider } from "./components/ui/provider";
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
